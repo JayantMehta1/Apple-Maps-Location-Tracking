073dcf5dc9992dbbe8493ae3035035f8ca54e068
Jayant Mehta <jay9mehta@gmail.com>
2020-12-04 19:44:16 -0500

    Location Updates Swift App - Followed Tutorial

---
+++ Location/ViewController.swift
@@ -0,0 +1,138 @@
+//imports
+
+import UIKit
+import CoreLocation
+import UserNotifications
+
+
+//class for viewController
+class ViewController: UIViewController, CLLocationManagerDelegate, UNUserNotificationCenterDelegate {
+
+    //variable to be used in Swift code from now on
+    let locationManager:CLLocationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        requestPermissionNotifications()
+                
+        locationManager.delegate = self
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        //initiates the updating process of the tutorials
+        locationManager.startUpdatingLocation()
+        
+        locationManager.distanceFilter = 100
+        
+        let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(43.61871, -116.214607), radius: 100, identifier: "Boise")
+        
+        locationManager.startMonitoring(for: geoFenceRegion)
+
+        //stop updating location here if needed as locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for currentLocation in locations{
+            print("\(index): \(currentLocation)")
+        }
+    }
+    
+  
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered: \(region.identifier)")
+        postLocalNotifications(eventTitle: "Entered: \(region.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited: \(region.identifier)")
+        postLocalNotifications(eventTitle: "Exited: \(region.identifier)")
+    }
+    
+    func requestPermissionNotifications(){
+        let application =  UIApplication.shared
+        
+        //specify that we are using ios 10 in this case
+        //use APNS to send notications to the display
+        //can be used to track when the bicycle location is changed, updating
+        //the user of potential secuirty issues
+
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { (isAuthorized, error) in
+                if( error != nil ){
+                    print(error!)
+                }
+                else{
+                    if( isAuthorized ){
+                        print("authorized")
+                        NotificationCenter.default.post(Notification(name: Notification.Name("AUTHORIZED")))
+                    }
+                    else{
+                        let pushPreference = UserDefaults.standard.bool(forKey: "PREF_PUSH_NOTIFICATIONS")
+                        if pushPreference == false {
+                            let alert = UIAlertController(title: "Turn on Notifications", message: "Push notifications are turned off.", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "Turn on notifications", style: .default, handler: { (alertAction) in
+                                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                                    return
+                                }
+                                
+                                if UIApplication.shared.canOpenURL(settingsUrl) {
+                                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                                        // Checking for setting is opened or not
+                                        print("Setting is opened: \(success)")
+                                    })
+                                }
+                                UserDefaults.standard.set(true, forKey: "PREF_PUSH_NOTIFICATIONS")
+                            }))
+                            alert.addAction(UIAlertAction(title: "No thanks.", style: .default, handler: { (actionAlert) in
+                                print("user denied")
+                                UserDefaults.standard.set(true, forKey: "PREF_PUSH_NOTIFICATIONS")
+                            }))
+                            let viewController = UIApplication.shared.keyWindow!.rootViewController
+                            DispatchQueue.main.async {
+                                viewController?.present(alert, animated: true, completion: nil)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+    }
+    
+    func postLocalNotifications(eventTitle:String){
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = eventTitle
+        content.body = "You've entered a new region"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let notificationRequest:UNNotificationRequest = UNNotificationRequest(identifier: "Region", content: content, trigger: trigger)
+        
+        center.add(notificationRequest, withCompletionHandler: { (error) in
+            if let error = error {
+                // Something went wrong
+                print(error)
+            }
+            else{
+                print("added")
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Biometrics-Bicycle-Lock-Location-Updates
+
+In addition to the C++ Arduino code for my biometrics bicycle lock project, I followed a Swift tutorial by Electronic Armory on how to get user locations and recieve notifications on location updates.
+
+The idea behind this is to recieve notifications when the location of a user's bicycle changes when they have put it in "parked" mode. Updating the user about their bicycle lock location will help them track where the bicycle is. 
+
+More importantly, it adds value to the fingerprint sensor security system with a notification system.
+
+An iOS location device would be embedded into the bicycle lock system and a cellular plan (similar to Apple Watches) can be purchased at low cost to utilize this bonus service.
